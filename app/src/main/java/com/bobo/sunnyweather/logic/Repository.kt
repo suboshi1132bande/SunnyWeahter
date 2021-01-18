98b70bf09639b0116e8b3c14dfc81f560f1d5151
suboshi1132bande <827578285@qq.com>
2021-01-18 21:53:38 +0800

    仓库层逻辑和viewmode

--- app/src/main/java/com/bobo/sunnyweather/logic/Repository.kt
+++ app/src/main/java/com/bobo/sunnyweather/logic/Repository.kt
@@ -1,4 +1,27 @@
 package com.bobo.sunnyweather.logic
 
+import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.liveData
+import com.bobo.sunnyweather.logic.model.Place
+import com.bobo.sunnyweather.logic.model.PlaceResponse
+import com.bobo.sunnyweather.logic.network.PlaceServcie
+import com.bobo.sunnyweather.logic.network.SunnyWheaterNetwork
+import java.lang.RuntimeException
+
 object Repository {
+
+    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            val paceResponse = SunnyWheaterNetwork.searchPlaces(query)
+            if (paceResponse.status  == "ok"){
+                val place = paceResponse.place
+                Result.success(place)
+            }else{
+                Result.failure(RuntimeException("response status is ${paceResponse.status}"))
+            }
+        }catch (e: Exception){
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
 }
---
+++ app/src/main/java/com/bobo/sunnyweather/ui/place/PlaceViewModel.kt
@@ -0,0 +1,23 @@
+package com.bobo.sunnyweather.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.bobo.sunnyweather.logic.Repository
+import com.bobo.sunnyweather.logic.model.Place
+import java.util.ArrayList
+
+class PlaceViewModel : ViewModel(){
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData){query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlace(query: String){
+        searchLiveData.value = query
+    }
+}
